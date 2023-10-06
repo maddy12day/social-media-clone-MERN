ee8c13343478aab2a4c879c962cf8f3c03292455
maddy12day <madhavdubey10@gmail.com>
2023-10-06 23:05:58 +0530

    frontend of the application

---
+++ client/src/components/closeFriend/CloseFriend.jsx
@@ -0,0 +1,11 @@
+import "./CloseFriend.scss";
+
+export const CloseFriend = ({user})=> {
+  return (<>
+    <li className="sidebarFriend">
+    <img className="sidebarFriendImg" src={user.profilePicture} alt="" />
+    <span className="sidebarFriendName">{user.username}</span>
+    </li>
+    </>
+  );
+};
---
+++ client/src/pages/profile/Profile.jsx
@@ -0,0 +1,39 @@
+import './Profile.scss'
+import { Feed } from "../../components/feed/Feed"
+import { Sidebar } from "../../components/sidebar/Sidebar"
+import { Topbar } from "../../components/topbar/topbar"
+import { Rightbar } from "../../components/rightbar/Rightbar"
+
+export const Profile = () => {
+    return (<>
+        <Topbar />
+        <div className="profile">
+            <Sidebar />
+            <div className="profileRight">
+                <div className="profileRightTop">
+                    <div className="profileCover">
+                        <img
+                            className="profileCoverImg"
+                            src="../../assets/post/3.jpeg"
+                            alt=""
+                        />
+                        <img
+                            className="profileUserImg"
+                            src="../../assets/person/7.jpeg"
+                            alt=""
+                        />
+                    </div>
+                    <div className="profileInfo">
+                        <h4 className="profileInfoName">Safak Kocaoglu</h4>
+                        <span className="profileInfoDesc">Hello my friends!</span>
+                    </div>
+                </div>
+                <div className="profileRightBottom">
+                    <Feed />
+                    <Rightbar profile />
+                </div>
+            </div>
+        </div>
+    </>
+    )
+}
